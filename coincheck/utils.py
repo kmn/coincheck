21b493d7a10f66df2dfe51b8ab4f7fb15217794c
Shohei KAMON <kamonshohei@gmail.com>
2015-04-08 23:35:46 +0900

    make account.py 'class'

--- coincheck/utils.py
+++ coincheck/utils.py
@@ -3,18 +3,18 @@ import hmac
 import hashlib
 import settings
 
-def make_header(url):
+def make_header(url,
+                access_key=None,
+                secret_key=None):
     ''' create request header function
     :param url: URL for the new :class:`Request` object.
     '''
     nonce = str(time.time()).split('.')[0]
-    key    = settings.access_key
-    secret = settings.secret_key
     url    = url
     message = nonce + url
-    signature = hmac.new(secret.encode('utf-8'), message.encode('utf-8'), hashlib.sha256).hexdigest()
+    signature = hmac.new(secret_key.encode('utf-8'), message.encode('utf-8'), hashlib.sha256).hexdigest()
     headers = {
-       'ACCESS-KEY'      : key,
+       'ACCESS-KEY'      : access_key,
        'ACCESS-NONCE'    : nonce,
        'ACCESS-SIGNATURE': signature
     }
