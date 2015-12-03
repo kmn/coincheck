b6518f917523ee50de8062de11456bea7b4b2281
Shohei KAMON <kamonshohei@gmail.com>
2015-12-03 19:04:57 +0900

    modify versions

--- coincheck/__init__.py
+++ coincheck/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'coincheck'
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 __author__ = 'Shohei Kamon'
 __license__ = 'MIT license'
 __copyright__ = 'Copyright 2015 Shohei Kamon'
