ea160a5d3d43acb037af620d25804be76ec89493
MYCOMPUTER\cat <totoronomori556@gmail.com>
2017-08-25 19:56:05 +0900

    update Market.trades() to be able to set offset

--- coincheck/market.py
+++ coincheck/market.py
@@ -16,25 +16,28 @@ class Market(object):
         pass
 
 
-    def public_api(self,url):
+    def public_api(self,url,offset=0):
         ''' template function of public api'''
+        params = {}
+        if(offset>0):
+            params = {'offset':offset}
         try :
             url in api_urls
-            return ast.literal_eval(requests.get(base_url + api_urls.get(url)).text)
+            return ast.literal_eval(requests.get(base_url + api_urls.get(url),params=params).text)
         except Exception as e:
             print(e)
-    
+
     def ticker(self):
         '''get latest information of coincheck market'''
-        return self.public_api('ticker') 
-    
-    def trades(self):
+        return self.public_api('ticker')
+
+    def trades(self,offset = 0):
         '''get latest deal history of coincheck market'''
-        return self.public_api('trades') 
-    
+        return self.public_api('trades',offset)
+
     def orderbooks(self):
         '''get latest asks/bids information of coincheck market'''
-        return self.public_api('order_books') 
+        return self.public_api('order_books')
 
 
 if __name__ == '__main__':
